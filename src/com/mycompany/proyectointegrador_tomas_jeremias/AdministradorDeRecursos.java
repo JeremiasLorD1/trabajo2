b4743ff4a6409f8502880e8e3bc3cc02fc14965f
JeremiasLorD1 <jeremiasmartinezwilhuber@gmail.com>
2023-10-20 18:56:37 -0300

    Falta el archivo 2

--- src/com/mycompany/proyectointegrador_tomas_jeremias/AdministradorDeRecursos.java
+++ src/com/mycompany/proyectointegrador_tomas_jeremias/AdministradorDeRecursos.java
@@ -6,7 +6,9 @@ package com.mycompany.proyectointegrador_tomas_jeremias;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  *
  * @author jerem
@@ -21,22 +23,70 @@ public class AdministradorDeRecursos {
     public AdministradorDeRecursos() {
         // Carga de Equipo.
         listaEquipo = new ArrayList<>();//esta lista es para llenar la lista de la interfaz
-
-        listaEquipo.add(new Equipo("Martillo", "Martilla", "Todas"));
-        listaEquipo.add(new Equipo("Tornillo", "Martisdadsalla", "Todasffsafdsa"));
-        listaEquipo.add(new Equipo("Rosca", "de tu ", ""));
-
+         try{
+            BufferedReader lector = new BufferedReader(new FileReader("equipos.txt"));
+            String linea;
+            int contador = 0;
+            String nombre = "";
+            String descripcion = "";
+            String areaEspecialidad = "";
+            while((linea=lector.readLine())!=null){
+                switch(contador){
+                    case 0:
+                        nombre=linea;
+                        break;
+                    case 1:
+                        descripcion=linea;
+                        break;
+                    case 2:
+                        areaEspecialidad=linea;
+                        listaEquipo.add(new Equipo(nombre,descripcion,areaEspecialidad));
+                }
+                contador=(contador+1)%3;
+            }
+            lector.close();
+         } catch(IOException e) {
+             e.printStackTrace();
+         }       
         // Carga de Cientiifico.//hacer la lectura de archivo de los equipos
         listaCientifico = new ArrayList<>();//esta lista es para llenar la lista de la interfaz
-
-        //no usar variables si no las uso ej c1 = jeremisa,martinez
-        listaCientifico.add(new Cientifico("Jeremias", "Martinez", "Doctor", "111"));
-        listaCientifico.add(new Cientifico("Facu", "Joaquin", "inge", "222"));
-        listaCientifico.add(new Cientifico("Capo", "Matias", "ijisdajidjsa", "333"));
-
-        for (Cientifico d : listaCientifico) {
-            contadorCientificos.put(d,0);
+        try {
+            BufferedReader lector= new BufferedReader(new FileReader("cientificos.txt"));
+            String linea;
+            int contador=0;
+            String nombre="";
+            String apellido="";
+            String especialidad="";
+            String dni="";
+            while((linea=lector.readLine())!=null){
+                switch(contador){
+                    case 0:
+                        nombre=linea;
+                        break;
+                    case 1:
+                        apellido=linea;
+                        break;
+                    case 2:
+                        especialidad=linea;                       
+                        break;
+                    case 3:
+                        dni=linea;
+                         listaCientifico.add(new Cientifico(nombre,apellido,especialidad,dni));
+                        break;                        
+                }
+                contador=(contador+1)%4;
+            }
+            lector.close();
+        } catch (Exception e) {
         }
+        //no usar variables si no las uso ej c1 = jeremisa,martinez
+        /*listaCientifico.add(new Cientifico("Jeremias", "Martinez", "Doctor", "111"));
+        listaCientifico.add(new Cientifico("Facu", "Joaquin", "inge", "222"));
+        listaCientifico.add(new Cientifico("Capo", "Matias", "ijisdajidjsa", "333"));*/
+
+       // for (Cientifico d : listaCientifico) {
+         //   contadorCientificos.put(d,0);
+        //}
 
     }
 
